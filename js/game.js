363bfca97563cf43d7b1b101631a27674e370823
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-03-11 03:44:32 -0800

    game canvas working

---
+++ js/game.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// Create a new scene
+let gameScene = new Phaser.Scene('Game');
+
+// set the configuration of the game
+let config = {
+  type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use canvas
+  width: 640,
+  height: 360,
+  scene: gameScene
+};
+
+// create a new game, pass the configuration
+let game = new Phaser.Game(config);
